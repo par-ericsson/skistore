f29fc47a84c094c5ecbf02aebfd7dbb7b8e483cb
Pär Ericsson <ericsson@home.se>
2022-06-01 19:44:05 +0200

    Added basket functionality on client.

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { BasketService } from './basket/basket.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -8,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
   title = 'Skistore';
 
-  constructor () {}
+  constructor (private basketService: BasketService) {}
 
   ngOnInit(): void {
-    
+    const basketId = localStorage.getItem('basket_id');
+    if (basketId) {
+      this.basketService.getBasket(basketId).subscribe(() => {
+        console.log('Initialized basket');
+      }, error => {
+        console.log(error);
+      })
+    }
   }
 }
