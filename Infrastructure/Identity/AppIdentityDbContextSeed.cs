3df935097f276887c73d3405f08e777950f190b8
Pär Ericsson <ericsson@home.se>
2022-06-08 19:38:08 +0200

    Identity added and AccountController, config

---
+++ Infrastructure/Identity/AppIdentityDbContextSeed.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Core.Entities.Identity;
+using Microsoft.AspNetCore.Identity;
+
+namespace Infrastructure.Identity
+{
+    public class AppIdentityDbContextSeed
+    {
+        public static async Task SeedUsersAsync(UserManager<AppUser> userManager)
+        {
+            if (!userManager.Users.Any())
+            {
+                var user = new AppUser
+                {
+                    DisplayName = "Bob",
+                    Email = "bob@test.com",
+                    UserName = "bob@test.com",
+                    Address = new Address
+                    {
+                        FirstName = "Bob",
+                        LastName = "Bobsson",
+                        Street = "The Street 10",
+                        City = "Gothenburg"
+                    }
+                };
+
+                await userManager.CreateAsync(user, "Pa$$w0rd");
+            }
+        }
+    }
+}
